1cf275f52ef072536e16e268339b8b2036e691f1
Kaj <tassi100@mail.chapman.edu>
2023-06-18 15:10:34 -0700

    user tesiting

--- nextjs-main/components/Hero/index.tsx
+++ nextjs-main/components/Hero/index.tsx
@@ -15,10 +15,10 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Free and collaborative surf forecasting website and content
+                Surf Forecasting and Content
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                Next Surf was made as an all in one surf forecasting website with reviews, content, and anything else you would look for in regards to your favorite hobby. We are San Diego based and hoping to grow and improve with your feedback. 
+                SurfnGo was made as an all in one surf forecasting website with reviews, content, and anything else you would look for in regards to your favorite hobby. We are San Diego based and hoping to grow and improve with your feedback. 
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
