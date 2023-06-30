2052709b47ad560079233e1f770667f78cb99f52
Kaj <tassi100@mail.chapman.edu>
2023-06-30 16:53:23 -0700

    new logo

--- nextjs-main/components/Header/index.tsx
+++ nextjs-main/components/Header/index.tsx
@@ -54,14 +54,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/logo-2.svg"
+                  src="/images/logo/SurfnGo.svg"
                   alt="NextSurf"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.svg"
+                  src="/images/logo/SurfnGo.svg"
                   alt="logo"
                   width={140}
                   height={30}
